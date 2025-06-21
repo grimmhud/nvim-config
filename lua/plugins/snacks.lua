13e6c6a58631b9500c0a77df2b1ba1126581611c
Hudson Oliveira <hud.others@proton.me>
2025-06-21 10:35:12 -0300

    init config

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,8 @@
+return {
+  {
+    "folke/snacks.nvim",
+    priority = 1000, -- carrega antes dos outros plugins
+    lazy = false,    -- carrega logo na inicialização
+    opts = {},       -- usa configuração padrão por enquanto
+  },
+}
