cf67bb7435f5ecd46bc9c421e11a21ad209c8e79
hudson Oliveira <hudsonoliveira@akkadiantech.com.br>
2025-06-25 19:06:30 -0300

    add keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Alternar buffers com Tab e Shift-Tab
+vim.keymap.set("n", "<Tab>", "<cmd>bnext<CR>", { desc = "Próximo buffer" })
+vim.keymap.set("n", "<S-Tab>", "<cmd>bprevious<CR>", { desc = "Buffer anterior" })
+
