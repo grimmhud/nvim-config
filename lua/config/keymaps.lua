c661bc30243934dc57f88441fa02ab8cb7f21d00
hudson Oliveira <hudsonoliveira@akkadiantech.com.br>
2025-06-27 18:15:52 -0300

    create keymap to terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,7 @@
 vim.keymap.set("n", "<Tab>", "<cmd>bnext<CR>", { desc = "Próximo buffer" })
 vim.keymap.set("n", "<S-Tab>", "<cmd>bprevious<CR>", { desc = "Buffer anterior" })
 
+-- Terminal space t
+-- INFO: Config on plugings/terminal.lua
+-- vim.keymap.set("n", "<C-t>f, "<cmd>ToggleTerm direction=float<CR>", { desc = "Terminal float" })
+-- vim.keymap.set("n", "<C-t>h, "<cmd>ToggleTerm direction=horizontal size=15<CR>", { desc = "Terminal horizontal" })
--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -2,12 +2,27 @@ return {
   {
     "akinsho/toggleterm.nvim",
     version = "*",
-    config = true,
     opts = {
       size = 15,
-      open_mapping = [[<c-\>]],
+      -- open_mapping = [[<c-\>]],
       direction = "float",
     },
+    config = function(_, opts)
+      require("toggleterm").setup(opts)
+
+      local Terminal = require("toggleterm.terminal").Terminal
+
+      local term1 = Terminal:new({ id = 1, direction = "horizontal", size = 15 })
+      local float_term = Terminal:new({ id = 2, direction = "float" })
+
+      -- Atalhos com <C-t>
+      vim.keymap.set("n", "<C-\\>h", function()
+        term1:toggle()
+      end, { desc = "Terminal horizontal" })
+
+      vim.keymap.set("n", "<C-\\>f", function()
+        float_term:toggle()
+      end, { desc = "Terminal flutuante" })
+    end,
   },
 }
-
